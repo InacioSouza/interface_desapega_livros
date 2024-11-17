763fed7832b2703ee16d2ada65333128edc95533
Inácio Souza Rocha <inaciosouzarocha69@gmail.com>
2024-11-16 22:47:07 -0300

    Componente de cabecalho oculto no componente de editar livro, nova configuração de rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LivroDetailComponent } from './components/livro-detail/livro-detail.component';
 import { ListaLivrosComponent } from './components/lista-livros/lista-livros.component';
+import { TelaHomeComponent } from './components/tela-home/tela-home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/listaLivro', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'livroDetail', component: LivroDetailComponent },
-  { path: 'listaLivro', component: ListaLivrosComponent },
+  { path: 'home', component: TelaHomeComponent, 
+    children: [
+      {path: 'listaLivro', component: ListaLivrosComponent}
+    ]
+  }
 ];
 
 @NgModule({
